85b4eb453e86b302bb7fc6146bbb4b616bffcda0
Colin G. Wilson <colingwilson7@gmail.com>
2020-02-22 10:36:56 -0500

    Ideating

--- app.R
+++ app.R
@@ -1,21 +1,28 @@
 library(shiny)
 library(auth0)
+library(pool)
+library(DBI)
 
 #
 pool <- dbPool(
   drv = RMariaDB::MariaDB(),
-  dbname = "fullsend",
+  dbname = "appdata",
   host = Sys.getenv("FULLSEND_HOST"),
   username = Sys.getenv("FULLSEND_USER"),
   password = Sys.getenv("FULLSEND_PASS"),
   port = 3306
 )
 
+DBI::dbGetQuery(pool, "select ST_AsText(blueprint) from gyms where gym_id=1")
+
 # simple UI with user info
 ui <- navbarPage(
   title = "Full Send",
   
   tabPanel("Routes",
+           imageOutput("gym_blueprint", height = 350,
+                       click = "image_click"
+                       )
            # shinyWidgets::switchInput(
            #   inputId = "",
            #   onLabel = "Sent!",
@@ -23,7 +30,7 @@ ui <- navbarPage(
            #   onStatus = 'success',
            #   offStatus = 'warning'
            # )
-           uiOutput
+           # uiOutput()
   ),
   
   tabPanel("User Info",
@@ -38,24 +45,60 @@ ui <- navbarPage(
 
 server <- function(input, output, session) {
   
-  current_routes <- reactive({
-      dbGetQuery(pool, "SELECT * FROM routes")
+  # Generate an image with black lines every 10 pixels
+  output$gym_blueprint <- renderImage({
+    # Get width and height of image output
+    width  <- session$clientData$output_gym_blueprint_width
+    height <- session$clientData$output_gym_blueprint_height
+    npixels <- width * height
+    
+    # Fill the pixels for R, G, B
+    m <- matrix(1, nrow = height, ncol = width)
+    # Add gray vertical and horizontal lines every 10 pixels
+    m[seq_len(ceiling(height/10)) * 10 - 9, ] <- 0.75
+    m[, seq_len(ceiling(width/10)) * 10 - 9]  <- 0.75
+    
+    # Convert the vector to an array with 3 planes
+    img <- array(c(m, m, m), dim = c(height, width, 3))
+    
+    # Write it to a temporary file
+    outfile <- tempfile(fileext = ".png")
+    writePNG(img, target = outfile)
+    
+    # Return a list containing information about the image
+    list(
+      src = outfile,
+      contentType = "image/png",
+      width = width,
+      height = height,
+      alt = "This is alternate text"
+    )
   })
   
-  output$routeInputButtons <- renderUI({
-    
-    routeList <- lapply(current_routes(), function(i) {
-      name <- paste("route", i, sep="")
-      shinyWidgets::switchInput(
-        inputId = ,
-        onLabel = "Sent!",
-        offLabel = "Did Not Send",
-        onStatus = 'success',
-        offStatus = 'warning'
-      )
-    })
-    do.call(tagList, unlist(routeList, recursive = FALSE))
+  output$click_info <- renderPrint({
+    cat("input$image_click:\n")
+    str(input$image_click)
   })
+  
+  current_routes <- reactive(input$image_click, {
+    dplyr::tbl(pool, routes) %>% 
+      mutate(d = sqrt((input$image_click$x - route_x)^2 + (input$image_click$y - route_y)^2)) %>% 
+      arrange(d)
+  })
+  
+  # output$route_switch_inputs <- renderUI({
+  #   route_list <- lapply(current_routes(), function(i) {
+  #     name <- paste("route", i, sep="")
+  #     shinyWidgets::switchInput(
+  #       inputId = ,
+  #       onLabel = "Sent!",
+  #       offLabel = "Did Not Send",
+  #       onStatus = 'success',
+  #       offStatus = 'warning'
+  #     )
+  #   })
+  #   do.call(tagList, unlist(routeList, recursive = FALSE))
+  # })
   
   # print user info
   output$user_info <- renderPrint({
